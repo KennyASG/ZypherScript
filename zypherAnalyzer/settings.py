c1caf61701428671bd888a53cc10a189b395adf2
KennyASG <saenzk031@gmail.com>
2024-05-10 23:46:09 -0600

    Analizador Lexico V1

--- zypherAnalyzer/settings.py
+++ zypherAnalyzer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--))ig_w-6eb!t)yw8kic2!vhcgezk1o09iwa6=3&a8qc5)4w1=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.zypherscript.site']
+ALLOWED_HOSTS = ['www.zypherscript.site','127.0.0.1']
 
 
 # Application definition
